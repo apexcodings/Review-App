53f7f348cf2f612eb57af48f7ec1d76b9f9306ba
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-05-27 09:21:00 -0400

    added sessions destroy method so that I can succesfully log out a user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,20 @@
 class SessionsController < ApplicationController
   
+  def new 
+
+  end
+
+  def create 
+
+  end
+
+  def destroy 
+    if session[:user_id] != nil 
+      session.delete :user_id
+    end
+    redirect_to root_url
+  end
+
+
+
 end
