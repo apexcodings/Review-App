72aa84f9cd9cdc533e1129a022ae817b7f204be0
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-05-27 09:10:03 -0400

    creating new method in user controller in order to test signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+  def home 
+
+  end
+
+  def index 
+    @users = User.all 
+  end
+
+  def new 
+    if !session[:user_id]
+      @user = User.new 
+    else  
+      @user = User.find(session[:user_id])
+      redirect_to user_path(@user)
+    end
+  end
+
+
+
+
 end
