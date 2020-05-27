4c8ffb2cf5bcf1084bd361d229f7bac636734cbd
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-05-27 09:38:55 -0400

    added session create so already existing users can successfully login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,13 @@ class SessionsController < ApplicationController
 
   end
 
-  def create 
-
+  def create
+    if @user = User.find_by(name: params[:session][:name])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to root_url
+    end
   end
 
   def destroy 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :courses, through: :reviews
   has_many :authors, through: :courses
   has_many :languages, through: :courses
+  validates :name, presence: true, uniqueness: true
 
   has_secure_password
 
