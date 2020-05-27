82619f8e02725ad00515d2ad0e12e67a20c8de4c
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-05-27 08:07:26 -0400

    creating helper methods in application controller for logged_in, current_user and require_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :require_login, :logged_in?, :current_user
+
+  def require_login 
+    unless session.include? :user_id 
+      redirect_to '/'
+    end
+  end
+
+  def logged_in?
+    current_user != nil
+  end
+
+  def current_user
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
+  end
+
+
 end
