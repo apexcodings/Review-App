dfa57d55cfc40eaa10d466e5c47bf4d768b56b64
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-05-28 16:51:30 -0400

    planning out main form for creating new review

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,4 +3,8 @@ class CoursesController < ApplicationController
   def new 
     @course = Course.new
   end
+
+  def create 
+    binding.pry
+  end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,27 @@ class Course < ApplicationRecord
   belongs_to :language
   has_many :reviews 
   has_many :users, through: :reviews
+
+  accepts_nested_attributes_for :reviews
+
+
+
+
+
+  def language_name=(name)
+    # can I not do self.language = Language.find_or_create_by(name: name)
+    language = Language.new(name: name)
+    self.language = language
+  end
+
+
+
+  author_name=
+
+
+
+  private 
+
+  strong_params permit the above two 
+
 end
