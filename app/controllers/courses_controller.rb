5b7902973b4abdf5454848f3f3b30a31a508043b
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-05-29 12:53:48 -0400

    added review association to course create action and now have functioning new course form

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -5,22 +5,23 @@ class CoursesController < ApplicationController
   end
 
   def create 
+    # binding.pry
     @course = Course.new(course_params)
+    @course.reviews.last.user = current_user
     if @course.save 
       redirect_to course_path(@course)
     else 
-      redirect_to course_path
+      redirect_to new_course_path
     end
   end
 
   def show 
     @course = Course.find(params[:id])
     @reviews = @course.reviews
-    @languages = @course.language
   end
 
   private 
   def course_params
-    params.require(:course).permit(:title, :level, :pages, :year_published, :type, :description, :author_name, :language_name)
+    params.require(:course).permit(:title, :level, :pages, :year_published, :type, :description, :author_name, :language_name, reviews_attributes: [:rating, :description])
   end
 end
