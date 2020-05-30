b04f4c876d40934af5707f1a93ff64c8a9d91328
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-05-30 08:17:33 -0400

    created show and index pages for authors/publishers so people can navigate to individual courses through them

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,4 +4,11 @@ class AuthorsController < ApplicationController
   def index 
     @authors = Author.all 
   end
+
+  def show 
+      @author = Author.find(params[:id])
+      @courses = @author.courses
+  end
+
+
 end
