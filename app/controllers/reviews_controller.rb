d05f7705d0522527a77e931760c75c08af7bf910
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-06-02 10:10:51 -0400

    added course_id and user_id to new action in reviews controller so that the review is succesful associated

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,10 +2,19 @@ class ReviewsController < ApplicationController
 
   def new 
     @course = Course.find(params[:course_id])
-    @review = Review.new
+    @review = Review.new(course_id: params[:course_id], user_id: current_user.id)
   end
 
   def create 
+    @review = Review.new(review_params)
+    @course = @review.course
+    @review.save 
+    redirect_to course_path(@course)
+  end
 
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :description, :user_id, :course_id)
   end
 end
