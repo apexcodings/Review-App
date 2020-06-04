50b3119c12240804253a2110614c0dab7f4daf29
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-06-04 07:51:36 -0400

    added email column to user model to help with omniauth login matchup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_160539) do
+ActiveRecord::Schema.define(version: 2020_06_04_114942) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_160539) do
     t.boolean "verified"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
