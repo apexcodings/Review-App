041d7a04a6687b4ead51873b18aa59fcc58f2344
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-06-07 12:15:56 -0400

    updated forms using partials and started looking at cleaning up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
 
   resources :courses do 
+    # Think I need most here as they are all nested
     resources :reviews
   end
 
+  # Not entirely sure I need any here
   resources :reviews, only: [:create, :update]
 
+  # Think I only need index and show, not sure about creation stuff
   resources :languages
 
+  # Same as language
   resources :authors
 
+  # Show page is all I have, don't need any CRUD, do I need routes for creation actions?
   resources :users
 
   root 'users#home'
@@ -17,6 +22,7 @@ Rails.application.routes.draw do
   get '/signup' => "users#new"
   post '/users' => "users#create"
   get '/login' => "sessions#new"
+  # Want to look at this one as I don't think it's best practice
   post '/' => "sessions#create"
   post '/logout' => "sessions#destroy"
 
