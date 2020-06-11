904d2b253c2d28cfb2ec3f155d3e87da6053bbd7
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-06-11 10:30:10 -0400

    adding random password generator to sessions_controller for users logging in through facebook

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 class SessionsController < ApplicationController
   
   def new 
@@ -10,7 +12,8 @@ class SessionsController < ApplicationController
     	session[:user_id] = @user.id
     	redirect_to user_path(@user)
     elsif auth && !User.find_by(email: auth[:info]['email'])
-      @user = User.new(name: auth[:info]['name'], email: auth[:info]['email'], password: "omniauth", password_confirmation: "omniauth")
+      secure_random_password = SecureRandom.hex(10)
+      @user = User.new(name: auth[:info]['name'], email: auth[:info]['email'], password: secure_random_password, password_confirmation: secure_random_password)
       @user.save
       session[:user_id] = @user.id
     	redirect_to user_path(@user)
